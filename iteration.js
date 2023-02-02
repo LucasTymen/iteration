41486ec9e42aa7e392b580a757cc789eff09134c
Lucas Tymen <lucas.tymen@gmail.com>
2023-02-02 11:40:25 +0100

    some_and_every_methods

--- iteration.js
+++ iteration.js
@@ -94,6 +94,29 @@ const newSum = newNumbers.reduce((accumulator, currentValue) => {
 },10);
 console.log(newSum)
 
+//############# .some & .every #############
+const words = ["unique", "uncanny", "pique", "oxymoron", "guise"];
+
+// Something is missing in the method call below
+
+console.log(
+  words.some((word) => {
+    return word.length < 6;
+  })
+);
+
+// Use filter to create a new array
+const interestingWords = words.filter((word) => word.length > 5);
+console.log(interestingWords);
+
+// Make sure to uncomment the code below and fix the incorrect code before running it
+
+console.log(
+  interestingWords.every((word) => {
+    return word.length > 5;
+  })
+);
+
 
 // ############## principles ##############
 /*
