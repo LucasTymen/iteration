bd0d49ef19404d55dea97b3b899f4f7e8d747cdd
Lucas Tymen <lucas.tymen@gmail.com>
2023-02-03 14:57:44 +0100

    execrcise_chose_the_right_operator

--- iteration.js
+++ iteration.js
@@ -142,8 +142,37 @@ const smallerNums = nums.map(num => num - 5);
 nums.some(num => num < 0);
 
 
-// ############## principles ##############
 /*
+//##################################################
+// ################## principles ###################
+//##################################################
+
+// ################### REVIEW ######################
+
+.forEach() is used to execute the same code on every element in an array but
+does not change the array and returns undefined.
+
+.map() executes the same code on every element in an array and returns a
+new array with the updated elements.
+
+.filter() checks every element in an array to see if it meets certain
+criteria and returns a new array with the elements that return truthy for
+the criteria.
+
+.findIndex() returns the index of the first element of an array that
+satisfies a condition in the callback function. It returns -1 if none of
+the elements in the array satisfies the condition.
+
+.reduce() iterates through an array and takes the values of the elements
+and returns a single value.
+
+All iterator methods take a callback function, which can be a pre-defined
+function, a function expression, or an arrow function.
+
+You can visit the Mozilla Developer Network to learn more about iterator
+methods (and all other parts of JavaScript!).
+
+
 different methods being called on the arrays:
 
     .forEach()
@@ -239,29 +268,5 @@ console.log([1, 2, 3].reduce((a, val) => a + val))
 // Résultat : 6
 // Explication : 1 + 2 + 3 = 6
 
-// ################### REVIEW ######################
-
-    .forEach() is used to execute the same code on every element in an array but
-    does not change the array and returns undefined.
-
-    .map() executes the same code on every element in an array and returns a
-    new array with the updated elements.
-
-    .filter() checks every element in an array to see if it meets certain
-    criteria and returns a new array with the elements that return truthy for
-    the criteria.
-
-    .findIndex() returns the index of the first element of an array that
-    satisfies a condition in the callback function. It returns -1 if none of
-    the elements in the array satisfies the condition.
-
-    .reduce() iterates through an array and takes the values of the elements
-    and returns a single value.
-
-    All iterator methods take a callback function, which can be a pre-defined
-    function, a function expression, or an arrow function.
-
-    You can visit the Mozilla Developer Network to learn more about iterator
-    methods (and all other parts of JavaScript!).
 
   */
